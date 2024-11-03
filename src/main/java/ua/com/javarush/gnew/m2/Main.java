575cee0f4b6986084678c4f4faa5fe8affb76edb
dRaider-bitrek <69346007+dRaider-bitrek@users.noreply.github.com>
2024-11-03 20:29:51 +0200

    CLI commands separated

--- src/main/java/ua/com/javarush/gnew/m2/Main.java
+++ src/main/java/ua/com/javarush/gnew/m2/Main.java
@@ -1,11 +1,6 @@
 package ua.com.javarush.gnew.m2;
 
-
-import picocli.CommandLine;
 import ua.com.javarush.gnew.m2.cli.PhoneBookCLI;
-import ua.com.javarush.gnew.m2.cli.commands.*;
-import ua.com.javarush.gnew.m2.service.PhoneBookInterface;
-import ua.com.javarush.gnew.m2.service.SimplePhoneBook;
 
 public class Main {
     public static void main(String[] args) {
