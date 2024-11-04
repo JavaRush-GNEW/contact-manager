509714f3928bc0030736b49c0981b9ecff2d6f6b
Denys <denisburduzan@gmail.com>
2024-11-04 02:54:22 +0200

    Add properties

---
+++ src/main/java/ua/com/javarush/gnew/m2/demo/JsonContactDtoRepository.java
@@ -0,0 +1,57 @@
+package ua.com.javarush.gnew.m2.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import ua.com.javarush.gnew.m2.dto.ContactDto;
+import ua.com.javarush.gnew.m2.dto.ContactDtoRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonContactDtoRepository implements ContactDtoRepository {
+
+   ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    @Override
+    public ContactDto load() throws IOException {
+        return objectMapper.readValue(new File("demo.st"), ContactDto.class);
+    }
+    @Override
+    public ContactDto findById(long id) throws IOException {
+         ContactDto contact = load();
+         return (contact.getId() == id) ? contact : null;
+    }
+    @Override
+    public void save(ContactDto contact) throws IOException {
+        objectMapper.writeValue(new File("demo.st"),contact);
+    }
+
+    public static void main(String[] args) {
+        JsonContactDtoRepository jsonContactDtoRepository=new JsonContactDtoRepository();
+
+
+        ContactDto contact = ContactDto.builder()
+                .id(1L)
+                .fullName("Иван Иванов")
+                .phones(List.of("+123456789"))
+                .emails(List.of("ivan@example.com"))
+                .build();
+
+
+        try {
+            jsonContactDtoRepository.save(contact);
+            System.out.println("Contact has been saved");
+
+            ContactDto lodList = jsonContactDtoRepository.load();
+            if(lodList != null){
+                System.out.println("saved contact" + lodList);
+            }
+            ContactDto foundContact = jsonContactDtoRepository.findById(1L);
+            System.out.println("Found contact: " + foundContact);
+
+        }catch (IOException e) {
+            System.err.println("Error while working with file: " + e.getMessage());
+        }
+    }
+
+}
--- src/main/java/ua/com/javarush/gnew/m2/dto/ContactDto.java
+++ src/main/java/ua/com/javarush/gnew/m2/dto/ContactDto.java
@@ -1,13 +1,15 @@
 package ua.com.javarush.gnew.m2.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
 @Builder
-public class ContactDto {
+public class ContactDto  {
     private long id;
 
     private String fullName;
@@ -15,5 +17,17 @@ public class ContactDto {
     private List<String> phones;
 
     private List<String> emails;
+
+    @JsonCreator
+    public ContactDto(@JsonProperty("id") long id,
+                      @JsonProperty("fullName") String fullName,
+                      @JsonProperty("phones") List<String> phones,
+                      @JsonProperty("emails") List<String> emails) {
+
+        this.id = id;
+        this.fullName = fullName;
+        this.phones = phones;
+        this.emails = emails;
+    }
 
 }
