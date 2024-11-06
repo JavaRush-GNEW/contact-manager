21cf9bce940ec8c3ba4ffb41797a59c9686be00d
dRaider-bitrek <69346007+dRaider-bitrek@users.noreply.github.com>
2024-11-06 22:55:48 +0200

    Add SettingsServiceInterface and SettingsService

--- src/main/java/ua/com/javarush/gnew/m2/Main.java
+++ src/main/java/ua/com/javarush/gnew/m2/Main.java
@@ -1,10 +1,9 @@
 package ua.com.javarush.gnew.m2;
 
-import java.io.IOException;
 import ua.com.javarush.gnew.m2.cli.PhoneBookCLI;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     PhoneBookCLI.init(args);
   }
 }
