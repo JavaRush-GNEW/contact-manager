ee6227efb480992f5a13aafeb44d4e2208a91df3
Denys <denisburduzan@gmail.com>
2024-11-08 05:26:17 +0200

    upgrd interface

--- src/main/java/ua/com/javarush/gnew/m2/service/SimplePhoneBook.java
+++ src/main/java/ua/com/javarush/gnew/m2/service/SimplePhoneBook.java
@@ -3,17 +3,31 @@ package ua.com.javarush.gnew.m2.service;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import ua.com.javarush.gnew.m2.dto.ContactDto;
+import java.util.stream.Collectors;
 
-public class SimplePhoneBook implements PhoneBookInterface {
+import lombok.SneakyThrows;
+import ua.com.javarush.gnew.m2.dto.ContactDto;
+import ua.com.javarush.gnew.m2.repository.ContactDtoRepository;
+
+public class SimplePhoneBook implements PhoneBookInterface, ContactDtoRepository {
   @Override
   public ContactDto add(ContactDto contactDto) {
-    return contactDto;
+   return ContactDto.builder()
+            .fullName(contactDto.getFullName())
+            .phones(List.of(String.valueOf(contactDto.getPhones())))
+            .emails(List.of(String.valueOf(contactDto.getEmails())))
+            .build();
   }
 
+  @SneakyThrows
   @Override
   public List<ContactDto> search(String str) {
-    return Collections.emptyList();
+    List<ContactDto> contacts = findAll();
+    return contacts.stream()
+            .filter(contact -> contact.getFullName().contains(str) ||
+                    contact.getPhones().stream().anyMatch(phones -> phones.contains(str)) ||
+                    contact.getEmails().stream().anyMatch(emails -> emails.contains(str)))
+            .collect(Collectors.toList());
   }
 
   @Override
