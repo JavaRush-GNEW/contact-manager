fdd7c4bbf84a9306bb3ae9fa2dd16c1b15eff589
Denys <denisburduzan@gmail.com>
2024-11-09 20:08:52 +0200

    проверка айди + уникальный айди

--- src/main/java/ua/com/javarush/gnew/m2/service/PhoneBookInterface.java
+++ src/main/java/ua/com/javarush/gnew/m2/service/PhoneBookInterface.java
@@ -1,14 +1,15 @@
 package ua.com.javarush.gnew.m2.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import ua.com.javarush.gnew.m2.dto.ContactDto;
 
 public interface PhoneBookInterface {
 
-  ContactDto add(ContactDto contactDto);
+  ContactDto add(ContactDto contactDto) throws IOException;
 
-  List<ContactDto> search(String str);
+  List<ContactDto> search(String str) throws IOException;
 
   void edit(ContactDto contact);
 
--- src/main/java/ua/com/javarush/gnew/m2/service/SimplePhoneBook.java
+++ src/main/java/ua/com/javarush/gnew/m2/service/SimplePhoneBook.java
@@ -1,19 +1,41 @@
 package ua.com.javarush.gnew.m2.service;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import ua.com.javarush.gnew.m2.dto.ContactDto;
+import ua.com.javarush.gnew.m2.repository.ContactDtoRepository;
+import ua.com.javarush.gnew.m2.repository.FileContactDtoRepository;
 
 public class SimplePhoneBook implements PhoneBookInterface {
+
+  ContactDtoRepository contactDtoRepository = new FileContactDtoRepository();
+
   @Override
-  public ContactDto add(ContactDto contactDto) {
+  public ContactDto add(ContactDto contactDto) throws IOException {
+    List<ContactDto> contacts = contactDtoRepository.findAll();
+    if (contactDto.getId() == 0) {
+      contactDto.setId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE);
+    }
+    contacts.add(contactDto);
+    contactDtoRepository.saveAll(contacts);
     return contactDto;
   }
 
   @Override
-  public List<ContactDto> search(String str) {
-    return Collections.emptyList();
+  public List<ContactDto> search(String str) throws IOException {
+    List<ContactDto> contacts = contactDtoRepository.findAll();
+    return contacts.stream()
+            .filter(
+                    contact ->
+                            contact.getFullName().contains(str)
+                                    || contact.getPhones().stream().anyMatch(phones -> phones.contains(str))
+                                    || contact.getEmails().stream().anyMatch(emails -> emails.contains(str)))
+            .collect(Collectors.toList());
   }
 
   @Override
