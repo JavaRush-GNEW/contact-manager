9c79e42dc4de3d4b9dc1ddd17420600a39cb074b
Denys <denisburduzan@gmail.com>
2024-11-09 15:16:48 +0200

    add class for interface

--- src/main/java/ua/com/javarush/gnew/m2/repository/ContactDtoRepository.java
+++ src/main/java/ua/com/javarush/gnew/m2/repository/ContactDtoRepository.java
@@ -12,46 +12,18 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import ua.com.javarush.gnew.m2.dto.ContactDto;
 
 public interface ContactDtoRepository {
-  ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-  default List<ContactDto> findAll() throws IOException {
-    File file = new File("demo.st");
-    if (!file.exists()) {
-      return new ArrayList<>();
-    }
-    return objectMapper.readValue(file, objectMapper.getTypeFactory().constructCollectionType(List.class, ContactDto.class));
-  }
+
+   List<ContactDto> findAll() throws IOException ;
 
 
-  default Optional<ContactDto> findById(long id) throws IOException {
-    return findAll().stream().filter(contact -> contact.getId() == id).findFirst();
-  }
+   Optional<ContactDto> findById(long id) throws IOException ;
 
-  default void deleteById(long id) throws IOException {
-    Optional<ContactDto> contactToDelete = findById(id);
-    if (contactToDelete.isPresent()) {
-      List<ContactDto> contacts = findAll();
-      contacts.remove(contactToDelete.get());
-      saveAll(contacts);
-    } else {
-      System.out.println("Контакт с id " + id + " не найден.");
-    }
-  }
+   void deleteById(long id) throws IOException;
 
-  default void saveAll(List<ContactDto> contacts) throws IOException {
-    objectMapper.writeValue(new File("demo.st"),contacts);
-  }
+   void saveAll(List<ContactDto> contacts) throws IOException;
 
-  default void save(ContactDto contactDto) throws IOException {
-    List<ContactDto> contacts = findAll();
-    contacts.add(contactDto);
-    saveAll(contacts);
-  }
-  default List<ContactDto> findByKeyword(String keyword) throws IOException {
-    List<ContactDto> contacts = findAll();
-    return contacts.stream()
-            .filter(contact -> contact.getFullName().contains(keyword) ||
-                    contact.getPhones().stream().anyMatch(phones -> phones.contains(keyword)) ||
-                    contact.getEmails().stream().anyMatch(emails -> emails.contains(keyword)))
-            .collect(Collectors.toList());
-  }
+   void save(ContactDto contactDto) throws IOException;
+
+   List<ContactDto> findByKeyword(String keyword) throws IOException ;
+
 }
