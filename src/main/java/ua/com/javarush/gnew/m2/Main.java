d8ef339e39acea623a6163c48233ac77494ed72a
Denys <denisburduzan@gmail.com>
2024-11-17 19:07:00 +0200

    Create a service class to access localized messages.

--- src/main/java/ua/com/javarush/gnew/m2/Main.java
+++ src/main/java/ua/com/javarush/gnew/m2/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import ua.com.javarush.gnew.m2.cli.PhoneBookCLI;
 import ua.com.javarush.gnew.m2.configuration.PhoneBookContext;
 
+
 public class Main {
   public static void main(String[] args) throws IOException {
     PhoneBookContext.create();
