34b98d40c219d24a0ccc00448586997184502979
Denys <denisburduzan@gmail.com>
2024-11-17 19:09:14 +0200

    Create a service class to access localized messages.

---
+++ src/main/java/ua/com/javarush/gnew/m2/service/LocalizationService.java
@@ -0,0 +1,25 @@
+package ua.com.javarush.gnew.m2.service;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LocalizationService {
+
+    private ResourceBundle resourceBundle;
+
+    public LocalizationService(Locale locale) {
+        setLocale(locale);
+    }
+
+    public void setLocale(Locale locale) {
+        this.resourceBundle = ResourceBundle.getBundle("messages", locale);  // messages.properties для каждого языка
+    }
+
+    public String getMessage(String key) {
+        try {
+            return resourceBundle.getString(key);
+        } catch (Exception e) {
+            return "Message not found for key: " + key;
+        }
+    }
+}
