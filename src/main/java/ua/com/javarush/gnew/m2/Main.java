42dcf5a4e511decabc59ec92b5b269c4feab8372
Sergiy Kachan <69346007+dRaider-bitrek@users.noreply.github.com>
2024-11-20 18:33:54 +0200

    Update Main.java

--- src/main/java/ua/com/javarush/gnew/m2/Main.java
+++ src/main/java/ua/com/javarush/gnew/m2/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import ua.com.javarush.gnew.m2.cli.PhoneBookCLI;
 import ua.com.javarush.gnew.m2.configuration.PhoneBookContext;
 
-
 public class Main {
   public static void main(String[] args) throws IOException {
     PhoneBookContext.create();
